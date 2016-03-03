d892a3f9fe90688964056c01754a08f1065ca4b4
Matt Sevrens <msevrens@gmail.com>
2016-03-03 13:52:36 -0800

    limit access to logged in users

--- modules/thought_quantifier/thought_quantifier_wordstream.tpl.php
+++ modules/thought_quantifier/thought_quantifier_wordstream.tpl.php
@@ -114,11 +114,13 @@
 </header>
 
 <div>
+  <?php if ($logged_in): ?>
   <?php print render($page['content']['thought_quantifier_word_stream_block']); ?>
-  <button class="btn btn-default form-submit" id="load-thoughts" name="" value="Load Thoughts">
-    Load Thoughts
-  </button>
-  <script>buildWordStream(streamData)</script>'
+    <button class="btn btn-default form-submit" id="load-thoughts" name="" value="Load Thoughts">
+      Load Thoughts
+    </button>
+    <script>buildWordStream(streamData)</script>
+  <?php endif; ?>
 </div>
 
 <footer class="footer container">
