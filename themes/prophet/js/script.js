90b45674e3cd8088912791dc98997a780e7abce7
Matt Sevrens <msevrens@gmail.com>
2017-02-17 21:05:26 -0800

    code for truth rating widget

--- themes/prophet/js/script.js
+++ themes/prophet/js/script.js
@@ -1,5 +1,40 @@
-jQuery(function() {
-	// Hack to hide trackable regex
-	jQuery("#edit-title-wrapper input").val("")
-	jQuery("#edit-title-wrapper input").css("color", "black");
-});
+prophet = (function($){
+
+	$(function() {
+
+		// Hack to hide trackable regex
+		$("#edit-title-wrapper input").val("")
+		$("#edit-title-wrapper input").css("color", "black");
+
+		// Truth Rating widget
+		$(".rate-fivestar-processed a").hover(
+			function() {
+				var index = $(this).closest(".rate-fivestar-processed").find("a").index($(this)),
+					parentField = $(this).closest(".views-field-value"),
+					labels = parentField.find(".views-label-value span"),
+					falseLabel = $(labels[0]),
+					trueLabel = $(labels[1]),
+					opacity = (index + 1) / 10,
+					inverseOpacity = 1 - opacity;
+			
+				trueLabel.css("opacity", opacity)
+				falseLabel.css("opacity", inverseOpacity)
+
+			}, function() {
+
+			}
+		);
+
+		$(".views-field-value").mouseleave(function() {
+			var labels = $(this).find(".views-label-value span"),
+				falseLabel = $(labels[0]),
+				trueLabel = $(labels[1]);
+
+			trueLabel.css("opacity", 1)
+			falseLabel.css("opacity", 1)
+
+		});
+
+	});
+
+})(jQuery);
